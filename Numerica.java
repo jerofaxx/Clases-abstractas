9f5d81aca4f02b877d06639b4d9cf923240fcd6a
Jeronimo <jerestrepora@unal.edu.co>
2025-01-18 19:37:00 -0500

    Delete Numerica.java

--- Numerica.java
+++
@@ -1,8 +0,0 @@
-public abstract class Numerica {
-    public abstract String toString();
-    public abstract boolean equals(Object ob);
-    public abstract Numerica sumar(Numerica numero);
-    public abstract Numerica restar(Numerica numero);
-    public abstract Numerica multiplicar(Numerica numero);
-    public abstract Numerica dividir(Numerica numero);
-}
